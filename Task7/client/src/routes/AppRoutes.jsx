dbe5979c178af444d43d12636a8a26d2a83e9380
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-05-19 19:00:51 +0530

    update home components

--- Task7/client/src/routes/AppRoutes.jsx
+++ Task7/client/src/routes/AppRoutes.jsx
@@ -8,7 +8,7 @@ const AppRoutes = () => {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
