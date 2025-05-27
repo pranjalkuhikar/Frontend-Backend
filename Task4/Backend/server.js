e6c2ec2b23d6600cc8ca6143416cdfcb4c38a25d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-05-27 19:04:19 +0530

    Remove Task 4

--- Task4/Backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import config from "./src/config/config.js";
-
-const PORT = config.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
