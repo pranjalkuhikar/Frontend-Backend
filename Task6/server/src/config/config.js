cc449c178f48ae005ff1aca05cad969bbc97ba59
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-05 11:46:51 +0530

    add user authentication methods and JWT token handling

--- Task6/server/src/config/config.js
+++ Task6/server/src/config/config.js
@@ -4,6 +4,8 @@ dotenv.config();
 const _config = {
   PORT: process.env.PORT,
   MONGO_URL: process.env.MONGO_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  EXPIRE: process.env.EXPIRE,
 };
 
 const config = Object.freeze(_config);
--- Task6/server/src/models/user.model.js
+++ Task6/server/src/models/user.model.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -16,6 +20,42 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
+
+userSchema.statics.hashPassword = async (password) => {
+  if (!password) {
+    throw new Error("Password is required");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+  if (!password) {
+    throw new Error("Password is required");
+  }
+  if (!this.password) {
+    throw new Error("Password is required");
+  }
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = jwt.sign({ id: this._id }, config.JWT_SECRET, {
+    expiresIn: config.EXPIRE,
+  });
+  return token;
+};
+
+userSchema.statics.verifyToken = async (token) => {
+  if (!token) {
+    throw new Error("Token is required");
+  }
+  const decoded = jwt.verify(token, config.JWT_SECRET);
+  return decoded;
+};
+
 const UserModel = mongoose.model("UserModel", userSchema);
 
 export default UserModel;
