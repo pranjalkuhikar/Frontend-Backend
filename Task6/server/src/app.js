8eaad8c6b0bd3dca2cb57f2c27d43ac8d9443585
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-05 12:19:44 +0530

    add user authentication services with registration and login functionality

--- Task6/server/src/app.js
+++ Task6/server/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import indexRouter from "./routes/index.route.js";
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- Task6/server/src/controllers/user.controller.js
+++ Task6/server/src/controllers/user.controller.js
@@ -1,7 +1,35 @@
-export const registerUser = (req, res) => {
-  res.status(201).json({ message: "User registered successfully" });
+import { loginService, registerService } from "../services/user.service.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await registerService({ username, email, password });
+    res.status(201).json({
+      status: "success",
+      message: "User registered successfully",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 };
 
-export const loginUser = (req, res) => {
-  res.status(200).json({ message: "Login User" });
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { user, token } = await loginService({ email, password });
+    res.status(200).json({
+      status: "success",
+      message: "Login successful",
+      data: { user, token },
+    });
+  } catch (error) {
+    res.status(401).json({
+      status: "error",
+      message: error.message,
+    });
+  }
 };
