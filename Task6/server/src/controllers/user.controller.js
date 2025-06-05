386ba83fdae888a791a8522a2718d6e555cdd3fd
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-06-05 11:17:30 +0530

    added message

--- Task6/server/src/controllers/user.controller.js
+++ Task6/server/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 export const registerUser = (req, res) => {
-  res.send("Register User");
+  res.status(201).json({ message: "User registered successfully" });
 };
 
 export const loginUser = (req, res) => {
-  res.send("Login User");
+  res.status(200).json({ message: "Login User" });
 };
